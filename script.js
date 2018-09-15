42e54409217846e4ec675f67850d56cc43447745
parmeet <parmeet.asi@gmail.com>
2018-09-15 16:27:44 +0530

    test website ready

---
+++ script.js
@@ -0,0 +1,7 @@
+const classToggle = () => {
+  const navs = document.querySelectorAll(".Navbar__Items");
+  navs.forEach(nav => nav.classList.toggle("Navbar__ToggleShow"));
+};
+document
+  .querySelector(".Navbar__Link-toggle")
+  .addEventListener("click", classToggle);
